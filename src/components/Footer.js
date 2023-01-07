a8bd74ecba925c53a2764b2a6a558392c55c0da2
henrykay <henrykay53@gmail.com>
2023-01-07 20:35:59 +0100

    Styled: Text input, Added: Icons to contact us section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,35 @@
 import React from 'react'
 // import './generalStyles.css'
 import '../styles/footer.css'
+import IG from '../images/ig.png'
+import TW from  '../images/tw.png'
+import FB from  '../images/fb.png'
+import IN from'../images/in.png'
+import SLK from '../images/slk.png'
+import PaperPlane from '../images/paperPlane.png'
 
 function Footer() {
   return (
     <div className='footer'>
         <h1 className='footerHeader'>Contact Us</h1>
 
-        <p className='footerPara'>lorem Lorem lorem</p>
+        <p className='footerPara'>lorem Lorem lorem lorem Lorem lorem <br></br>
+        lorem Lorem lorem</p>
 
         <form className='inputForm'>
         <input type="text" placeholder='Enter your email address'></input>
+        <img  className= "paperPlane" src={PaperPlane} alt="Paper plane"></img>
 
         </form>
 
+        <div className='socials'>
+            <img src={IG} alt="Instagram" />
+            <img src={TW} alt="Twitter" />
+            <img src={FB} alt="Facebook" />
+            <img src={IN} alt="LinkedIn" />
+            <img src={SLK} alt="Slack" />
 
-        
-
-        
+        </div>
 
     </div>
   )
