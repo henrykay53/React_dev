0cd536d39018faf1298f28dcbbbf1ce0a4260eb2
henrykay <henrykay53@gmail.com>
2023-01-07 19:25:31 +0100

    Footer layout and styling almost complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,25 @@
 import React from 'react'
+// import './generalStyles.css'
+import '../styles/footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <h1 className='footerHeader'>Contact Us</h1>
+
+        <p className='footerPara'>lorem Lorem lorem</p>
+
+        <form className='inputForm'>
+        <input type="text" placeholder='Enter your email address'></input>
+
+        </form>
+
+
+        
+
+        
+
+    </div>
   )
 }
 
