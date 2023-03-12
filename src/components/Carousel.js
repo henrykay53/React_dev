40344b9f00602c0a18e5ed5d21cc899c7c171206
henrykay <henrykay53@gmail.com>
2023-03-12 19:53:21 +0100

    Carousel logic implemented

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../styles/Carousel.css"
 import JN from "../images/janella.png"
 import PREV from "../images/prev.png"
 import NEXT from "../images/next.png"
 
 export function Carousel() {
+
+  const [currentCarousel, setCurrentCarousel] = useState(1);
+
+  const Increase = ()=> {
+    if(currentCarousel === 3) {
+      setCurrentCarousel(1)
+    }
+
+    else {
+      setCurrentCarousel(currentCarousel + 1)
+    }
+  
+  }
+
+  const Decrease = ()=> {
+    if(currentCarousel > 1) {
+      setCurrentCarousel(currentCarousel -1)
+    }
+
+    else {
+      setCurrentCarousel(1)
+    }
+
+  }
+
+
   return (
     <div className="carousel">
 
@@ -13,20 +39,33 @@ export function Carousel() {
          <p>Loren loren loren loren lorem lorem</p>
       </div>
      
-
       <div className="carousel-container">
-        <div className='prev-button'>
-          <img className='prev' src={PREV} alt="previous-button"/>
-        </div>
+      
 
-        <div className="carousel-one">
-          <p>Loren lorem lorem lorem lorem lorem lorem
-            lorem lorem lorem lorem lorem lorem loremlorem lorem lorem lorem
-            lorem lorem lorem
+        {currentCarousel === 1 && <div className="carousel-one">
+          <p>Loren lorem lorem lorem lorem 
           </p>
 
 
-          <div className="profile-flex">
+          <div className="profile-flex-one">
+
+            <img src={JN} alt="user-icon"/>
+            <div className='profile-name, black'>
+              <p>Janella</p>
+              <p>LUTON</p>
+            </div>
+
+          </div>
+
+        </div>}
+
+        {currentCarousel === 2 && <div className="carousel-two">
+          <p>Loren lorem lorem lorem lorem lorem lorem
+            lorem lorem lorem lorem l
+          </p>
+
+
+          <div className="profile-flex-one">
 
             <img src={JN} alt="user-icon"/>
             <div className='profile-name, black'>
@@ -39,40 +78,17 @@ export function Carousel() {
          
   
 
-        </div>
+        </div>}
 
+        {currentCarousel === 3 && <div className="carousel-three">
 
-        <div className="carousel-two">
-        <p>Loren lorem lorem lorem lorem lorem lorem
+          <p>Loren lorem lorem lorem lorem lorem lorem
             lorem lorem lorem lorem lorem lorem loremlorem lorem lorem lorem
             lorem lorem lorem
           </p>
 
-      
 
-          <div className="profile-flex">
-
-            <img src={JN} alt="user-icon"/>
-            <div className='profile-name'>
-              <p>Janella</p>
-              <p>LUTON</p>
-            </div>
-
-          </div>
-          
-
-        </div>
-
-
-        <div className="carousel-three">
-        <p>Loren lorem lorem lorem lorem lorem lorem
-            lorem lorem lorem lorem lorem lorem loremlorem lorem lorem lorem
-            lorem lorem lorem
-          </p>
-
-         
-
-          <div className="profile-flex">
+          <div className="profile-flex-one">
 
             <img src={JN} alt="user-icon"/>
             <div className='profile-name, black'>
@@ -81,11 +97,16 @@ export function Carousel() {
             </div>
 
           </div>
-          
+
+        </div>}
+        
+
+        <div className='prev-button' onClick={()=> Decrease()}>
+          <img className='prev' src={PREV} alt="previous-button"/>
         </div>
 
 
-        <div className='next-button'>
+        <div className='next-button' onClick={()=> Increase()}>
           <img className='next' src={NEXT} alt="next-button"/>
         </div>
 
